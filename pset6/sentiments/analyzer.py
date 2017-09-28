381dc71cbc73484ae63f08aeaac326b1fcf75e0c
Erik Benton <>
2017-09-28 01:31:39 +0000

    pset6 sentiments, add positive and negative lists to analyzer.py

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -6,10 +6,21 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
-        # TODO
+        self.positives = []
+        self.negatives = []
+        self.read_words(positives, self.positives)
+        self.read_words(negatives, self.negatives)
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
         # TODO
         return 0
+
+    def read_words(self, text, list):
+        with open(text,'r') as f:
+            for line in f:
+                for word in line.split('\n'):
+                    if(word.startswith(';') == False):
+                        list.append(word.strip())
+                        print(word.strip('\n'))
